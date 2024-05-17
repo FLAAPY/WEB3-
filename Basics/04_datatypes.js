6ebce4df492d5d9c513b2cf6f592cc717ede33f4
flappy <165365346+FLAAPY@users.noreply.github.com>
2024-05-17 09:53:21 +0000

    datatypes

---
+++ Basics/04_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as a newer version 
+// alert (3+3) this will create a pop up with the result and the 
+//user need to click the ok button 
+
+// number =  2 raise to power 53
+// bigint = for biogger data types 
+//  string = ""
+// boolean = true / false
+// null = stand alone value 
+// undefiend = value not assigned 
+// symbol = unique ness 
+
+// object 
+console.log(typeof undefined); // answer : undefined !
+console.log(typeof null); // answer : object !
