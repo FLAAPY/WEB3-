3dc97cac9bd31e8cff760f34de11b36bcd864493
Flaapy <165365346+FLAAPY@users.noreply.github.com>
2024-05-18 05:32:45 +0000

    data type conversion

---
+++ Basics/05_datatype conversion.js
@@ -0,0 +1,12 @@
+let playerScore = "456" // defining a number input 
+console.log(typeof playerScore); // finding out the type of datatype ?
+console.log(typeof (playerScore)); // another method to find out
+let valueInnumber = Number(playerScore) // defining contraint that the value should be a number 
+console.log(typeof valueInnumber); // printing it
+console.log(valueInnumber);
+
+// boolean conversion 
+// 1 = true : 0 = false 
+// "" = false
+// "flaapy" =  true
+
