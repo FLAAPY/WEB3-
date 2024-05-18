6be90ae678a706af38b2f08c540e225b78269585
Flaapy <165365346+FLAAPY@users.noreply.github.com>
2024-05-18 05:59:14 +0000

    comparison

---
+++ Basics/07_comparisons.js
@@ -0,0 +1,21 @@
+// some basic comparisons
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+console.log("2" > 1); // auto conversion of datatype
+console.log("02"> 1);
+// datatype must be same while comparing 
+console.log(null<0);
+console.log(null==0);
+console.log(null >=0);
+
+// comparison convert null to a number
+// treating it a 0 therefore null >= 0 is true and null >= is false
+
+// === strict check this will also check the datatype of the conversion 
+console.log("2" === 2 ); // answer would be false 
