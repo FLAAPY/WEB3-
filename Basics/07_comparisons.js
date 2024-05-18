b489efd4239794849b241cd1dbd7b0f28bfa439e
Flaapy <165365346+FLAAPY@users.noreply.github.com>
2024-05-18 06:13:20 +0000

    update

--- Basics/07_comparisons.js
+++ Basics/07_comparisons.js
@@ -19,3 +19,25 @@ console.log(null >=0);
 
 // === strict check this will also check the datatype of the conversion 
 console.log("2" === 2 ); // answer would be false 
+
+//  Primitive data type (copied from memory and then given to you )
+// 7 types : String , Number , Boolean , Null , undefined , symbol , BigInt
+
+// 
+
+// Reference data type (non primitive) (direct reference is give to you)
+// array , objects , functions 
+
+const heros = ["flaapy", "flaapy2"] //array
+let objects = {
+    name : "flaapy",
+    age : 01,
+} // {} inside this is object 
+
+// function
+const myFunction = function(){
+    console.log("hi flaapy");
+}    
+// typeof is also a function 
+// typeof function return function (function object)
+// typeof symbol is symbol
