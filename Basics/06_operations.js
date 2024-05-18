564986d635548ce64d76465f4a1069d9bd366845
Flaapy <165365346+FLAAPY@users.noreply.github.com>
2024-05-18 05:48:07 +0000

    operations

---
+++ Basics/06_operations.js
@@ -0,0 +1,17 @@
+let value = 456
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);*/
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"3"); // this is something different output asnwer could be found at ecma script 
+ 
+// link : https://tc39.es/ecma262/
