bb26fe8dbd9d9fbfd5f3667d41f18f21a8dc8215
Flaapy <165365346+FLAAPY@users.noreply.github.com>
2024-06-22 10:26:17 +0000

    loop

---
+++ Javascript Iterations/01_loops.js
@@ -0,0 +1,28 @@
+// // for loop 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element ==5) {
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 0; j <=10; j++) {
+//     //console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     console.log(i + '*' + j + '=' + i*j);
+//     }
+// }
+
+
+// let myarray = ["flaapy", "superman" ,  "batman"] // defining own array 
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);   
+// }
+
+// break and continue 
+// break can be used to break the control flow , and come out of the loop value and come out of the scope 
+// continue for the first time the condition is skipped and the code move forward as usual but we are still in the same scope 
+
---
+++ Javascript Iterations/02_whileloop.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index <= 10) {
+    console.log(`value of the index is ${index}`);
+    index = index + 2   
+}
+
+let myArray = ["flaap1", "flaapy2", "flaapy3"]
+let arr = 0 
+while (arr < myArray.length ) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+} while (score <= 10);
